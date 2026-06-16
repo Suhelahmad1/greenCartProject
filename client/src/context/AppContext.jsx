15ee691e9901a3f4eaf7b3fcac264c6765275740
Suhel Ahmad <ad.sohail1425@gmail.com>
2026-06-16 14:37:00 +0530

    Fix axios baseURL for production

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,7 +4,7 @@ import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = "https://greencart-backend-chi-eight.vercel.app";
 axios.defaults.withCredentials = true;
 
 export const AppContext = createContext();
